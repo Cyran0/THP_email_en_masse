1ca52d6a178bc4bfb2317ea54a79234dfeab6562
recep54 <recep54@outlook.fr>
2018-10-18 18:30:57 +0200

    branche receptionniste terminée, il faut dussionf

--- lib/townhalls_mailer.rb
+++ lib/townhalls_mailer.rb
@@ -38,4 +38,29 @@ end
 
 #=======================================================================================================================
 
+k = 0
 
+while k < @email.size
+
+  mail = @email[k]
+  city = @name[k]
+
+  gmail = Gmail.connect(ENV["email"],ENV["mdp"])
+
+  gmail.deliver do
+    to mail
+    subject "BG du 54"
+    text_part do
+      content_type 'text; charset=UTF-8'
+      body "Bonjour,
+Je m'appelle Henri, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+
+Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{city} veut changer le monde avec nous ?
+
+
+Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
+  end
+
+  end
+  k += 1
+end
